b10c5b1abfed7837eb85928a0353987737a1b5ba
Akshat <akshatjain.ms@gmail.com>
2023-03-28 23:30:12 +0530

    Check in Main file

--- Tic-Tac-Toe/main.py
+++ Tic-Tac-Toe/main.py
@@ -2,6 +2,8 @@ import pygame as pg
 import tictactoe
 import sys
 
+pg.init()
+
 # Define Windoe Size
 WIN_SIZE = 900
 
@@ -10,26 +12,31 @@ class Game:
 
     # Description - Constructor or Initializer
     def __init__(self):
-
+        
         # Setup the drawing window.
-        self.screen = pg.display.set_mode((WIN_SIZE, WIN_SIZE))
+        self.screen = pg.display.set_mode([WIN_SIZE] * 2) 
 
         #instant of Clock class to set the framerate.
         self.clock = pg.time.Clock()
 
         # Create class contains main logic of the Game.
         self.tic_tac_toe = tictactoe.TicTacToe(self)
+    
+    def new_game(self):
+        self.tic_tac_toe = tictactoe.TicTacToe(self)
 
     # Description - Method to check Evnets.
     def check_events(self):
 
         # Checking for the events. 
-
         # Check for Quit Event.
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
                 sys.exit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_SPACE:
+                    self.new_game()
 
     # Description - Application Launch Method.
     def run_game(self):
